b2daae2a9e8f4239aa08619fba2c1ae6ebf21a98
Jake Feala <jake@outlierbio.com>
2017-02-23 11:30:36 -0500

    Working RNA-seq pipeline
    Working and tested RNA-seq pipeline. Pipeline code and testing makes
    some assumptions about an Airtable database. Test assumes test dataset
    is assigned ID `GE-1`

--- ob_pipelines/pipelines/rnaseq.py
+++ ob_pipelines/pipelines/rnaseq.py
@@ -1,16 +1,15 @@
 import logging
 import os
 import os.path as op
-import re
-from subprocess import check_output
-from tempfile import mkdtemp, mkstemp
 
 from luigi.s3 import S3Target, S3PathTask
-from luigi import Parameter, Task, WrapperTask, ExternalTask
+from luigi import Parameter, BoolParameter, Task, WrapperTask
 import requests
 
-from ob_pipelines.s3 import s3, path_to_bucket_and_key
+from ob_pipelines.s3 import csv_to_s3
+from ob_pipelines.batch import BatchTask
 from ob_pipelines.apps.star import STAR_OUTPUTS
+from ob_pipelines.apps.kallisto import merge_column
 
 logger = logging.getLogger('luigi-interface')
 
@@ -26,24 +25,18 @@ DOCKER_RUN = [
 THREADS = 2
 
 
-def run_logged(cmd):
-    """Wrap subprocess.check_output with logs"""    
-    logger.info('Running: {}'.format(' '.join(cmd)))
-    out = check_output(cmd)
-    logger.info(out.decode())
-
 
 def get_index(tool, species='human', build='latest'):
     indexes = {
         'star': {
             'human': {
-                'test': '/scratch/star.b38.chr21',
+                'test': '/reference/star/b38.chr21',
                 'latest': '/reference/star/b38.gencode_v25.101'
             }
         },
         'kallisto': {
             'human': {
-                'test': '/scratch/gencode_v25.chr21.idx',
+                'test': '/reference/kallisto/gencode_v25.chr21/gencode_v25.chr21.idx',
                 'latest': '/reference/kallisto/gencode_v25/gencode_v25.idx'
             }
         }
@@ -64,7 +57,7 @@ def _request(method, table, path, **kwargs):
 
 def get_sample_by_key(key):
     """Retrieve Samples record by key from Airtable API"""
-    return _request('get', 'Genomics%20Samples', '/' + key)
+    return _request('get', 'Genomics%20Sample', '/' + key)
 
 
 def get_sample(sample_id):
@@ -72,13 +65,13 @@ def get_sample(sample_id):
     params = {
         'filterByFormula': '{Name} = "%s"' % sample_id,
     }
-    records = _request('get', 'Genomics%20Samples', '/', params=params)['records']
+    records = _request('get', 'Genomics%20Sample', '/', params=params)['records']
     return records[0]
 
 
 def get_experiment_by_key(key):
     """Retrieve Experiment record by key from Airtable API"""
-    return _request('get', 'Genomics%20Expts', '/' + key)
+    return _request('get', 'Genomics%20Expt', '/' + key)
 
 
 def get_experiment(expt_id):
@@ -86,7 +79,15 @@ def get_experiment(expt_id):
     params = {
         'filterByFormula': '{Name} = "%s"' % expt_id,
     }
-    return _request('get', 'Genomics%20Expts', '/', params=params)['records'][0]
+    return _request('get', 'Genomics%20Expt', '/', params=params)['records'][0]
+
+
+def get_samples(expt_id):
+    sample_keys = get_experiment(expt_id)['fields']['Genomics samples']
+
+    for sample_key in sample_keys:
+        sample = get_sample_by_key(sample_key)
+        yield sample['fields']['Name']
 
 
 class Sample(object):
@@ -113,8 +114,20 @@ class Sample(object):
         return self._experiment
 
 
+class PipelineTask(BatchTask):
 
-class FastQC(Task, Sample):
+    local = BoolParameter(False, significant=False)
+
+    @property
+    def job_name(self):
+        return '{}-{}'.format(self.task_family, self.sample_id)
+
+
+class FastQC(PipelineTask, Sample):
+
+    job_definition = 'fastqc'
+    command = ['Ref::fq1', 'Ref::fq2', 'Ref::out_dir', 'Ref::name']
+    image = 'outlierbio/fastqc'
 
     def requires(self):
         return (S3PathTask(path=self.sample['FastQ 1']),
@@ -130,21 +143,40 @@ class FastQC(Task, Sample):
         return {k: S3Target('s3://{}/{}/fastqc/{}'.format(S3_BUCKET, self.sample_folder, fname))
                 for k, fname in s3_paths.items()}
 
-    def run(self):
+    @property
+    def parameters(self):
         fq1, fq2 = [tgt.path for tgt in self.input()]
         out_dir = op.dirname(self.output()['html_1'].path) + '/'
-
-        cmd = DOCKER_RUN + ['outlierbio/fastqc', fq1, fq2, out_dir, self.sample_id]
-        run_logged(cmd)
+        return {
+            'fq1': fq1,
+            'fq2': fq2,
+            'out_dir': out_dir,
+            'name': self.sample_id
+        }
 
 
 class UpdateFastQC(Task, Sample):
     pass
 
 
-class Star(Task, Sample):
+class Star(PipelineTask, Sample):
+
+    job_definition = 'star'
+    image = 'outlierbio/star'
+    command = ["star_align", "Ref::fq1", "Ref::fq2", "Ref::genome_dir", 
+               "Ref::prefix", "Ref::threads"]
 
     @property
+    def parameters(self):
+        fq1, fq2 = [tgt.path for tgt in self.input()]
+        return {
+            'fq1': fq1,
+            'fq2': fq2,
+            'genome_dir': get_index('star'),
+            'prefix': self.prefix(),
+            'threads': '5' if self.local else '20'
+        }
+
     def prefix(self):        
         return 's3://{}/{}/star/{}'.format(
             S3_BUCKET, self.sample_folder, self.sample_id + '.')
@@ -154,82 +186,138 @@ class Star(Task, Sample):
                 S3PathTask(path=self.sample['FastQ 2']))
 
     def output(self):
-        s3_paths = {k: v.format(prefix=self.prefix) for k, v in STAR_OUTPUTS.items()}
+        s3_paths = {k: v.format(prefix=self.prefix()) for k, v in STAR_OUTPUTS.items()}
         return {k: S3Target(path) for k, path in s3_paths.items()}
 
-    def run(self):
-        fq1, fq2 = [tgt.path for tgt in self.input()]
 
-        cmd = DOCKER_RUN + [
-            'outlierbio/star', 
-            fq1, fq2, 
-            get_index('star', build='test'), 
-            self.prefix, 
-            str(THREADS)
-        ]
-        run_logged(cmd)
+class IndexBam(PipelineTask, Sample):
 
+    job_definition = 'samtools-index'
+    image = 'outlierbio/samtools'
+    command = ['index', 'Ref::input', 'Ref::output']
 
-class IndexBam(Task, Sample):
+    @property
+    def parameters(self):
+        return {
+            'input': self.input()['bam'].path, 
+            'output': self.output().path
+        }
 
     def requires(self):
         return Star(sample_id=self.sample_id)
 
     def output(self):
-        return self.input().path + '.bai'
-
-    def run(self):
-        cmd = DOCKER_RUN + [
-            'outlierbio/samtools', 'index', 
-            self.input()['bam'].path, 
-            self.output().path
-            ]
-
-        run_logged(cmd)
+        return S3Target(self.input()['bam'].path + '.bai')
 
 
-class Kallisto(Task, Sample):
+class GeneCoverage(BatchTask, Sample):
+
+    job_definition = 'gene-coverage'
+    image = 'outlierbio/rseqc'
+    command = [
+        'geneBody_coverage.py',
+        '-r', 'Ref::bedfile',
+        '-i', 'Ref::input', 
+        '-o', 'Ref::output'
+    ]
+
+    @property
+    def parameters(self):
+        return {
+            'bedfile': '/hg38.housekeeping.bed',
+            'input': self.input()['bam'].path, 
+            'output': self.output().path
+        }
+
+    def requires(self):
+        return Star(sample_id=self.sample_id)
+
+    def output(self):
+        return S3Target('s3://{}/{}/rseqc/geneBody_coverage.png'.format(
+            S3_BUCKET, self.sample_folder))
+
+
+class Kallisto(PipelineTask, Sample):
+    
+    job_definition = 'kallisto'
+    image = 'outlierbio/kallisto'
+    command = [
+            "quant",
+            "-i", "Ref::index",
+            "-o", "Ref::output",
+            "-t", "Ref::threads",
+            "Ref::reads1", 
+            "Ref::reads2"
+    ]
+
+    @property
+    def parameters(self):
+        fq1, fq2 = self.input()
+        return {
+            'threads': '20',
+            'index': get_index('kallisto'),
+            'reads1': fq1.path,
+            'reads2': fq2.path,
+            'output': op.dirname(self.output()['abundance'].path) + '/'
+        }
 
     def requires(self):
         return (S3PathTask(path=self.sample['FastQ 1']),
                 S3PathTask(path=self.sample['FastQ 2']))
 
     def output(self):
-        s3_paths = {
-            'abundance': 'abundance.tsv',
-            'h5': 'abundance.h5',
-            'run_info': 'run_info.json'
+        output_files = {
+                'abundance': 'abundance.tsv',
+                'h5': 'abundance.h5',
+                'run_info': 'run_info.json'
         }
         return {k: S3Target('s3://{}/{}/kallisto/{}'.format(S3_BUCKET, self.sample_folder, fname))
-                for k, fname in s3_paths.items()}
+                for k, fname in output_files.items()}
+
+
+class MergeKallisto(Task):
+
+    expt_id = Parameter()
+
+    def requires(self):
+        return {
+            sample_id: Kallisto(sample_id=sample_id)
+            for sample_id in get_samples(self.expt_id)
+        }
+
+    def output(self):
+        prefix = 's3://{}/{}/'.format(S3_BUCKET, self.expt_id)
+        return {
+            'annotations': S3Target(prefix + 'annotations.csv'),
+            'est_counts': S3Target(prefix + 'est_counts.csv'),
+            'tpm': S3Target(prefix + 'tpm.csv')
+        }
 
     def run(self):
-        fq1, fq2 = [tgt.path for tgt in self.input()]
-        out_dir = op.dirname(self.output()['abundance'].path) + '/'
+        # Gather input filepaths and labels
+        tgt_dict = self.input()
+        sample_ids = list(tgt_dict.keys())
+        fpaths = [tgt_dict[sample_id]['abundance'].path for sample_id in sample_ids]
 
-        cmd = DOCKER_RUN + [
-            'outlierbio/kallisto', 'quant',
-            '-i', get_index('kallisto', build='test'), 
-            '-o', out_dir,
-            fq1, fq2
-        ]
-        run_logged(cmd)
-        
+        # Merge columns
+        annotations, est_counts = merge_column(fpaths, sample_ids, data_col='est_counts')
+        annotations, tpm = merge_column(fpaths, sample_ids, data_col='tpm')
+
+        csv_to_s3(annotations, self.output()['annotations'].path)
+        csv_to_s3(est_counts, self.output()['est_counts'].path)
+        csv_to_s3(tpm, self.output()['tpm'].path)
+
 
 class RnaSeq(WrapperTask):
 
     expt_id = Parameter()
 
     def requires(self):
-        sample_keys = get_experiment(self.expt_id)['fields']['Genomics samples']
-
-        for sample_key in sample_keys:
-            sample = get_sample_by_key(sample_key)
-            sample_id = sample['fields']['Name']
-
+        for sample_id in get_samples(self.expt_id):
             yield FastQC(sample_id=sample_id)
             yield Star(sample_id=sample_id)
+            yield IndexBam(sample_id=sample_id)
             yield Kallisto(sample_id=sample_id)
-
+            yield GeneCoverage(sample_id=sample_id)
 
 
---
+++ test/test_rnaseq.py
@@ -0,0 +1,17 @@
+import boto3
+import luigi
+
+from ob_pipelines.s3 import path_to_bucket_and_key
+from ob_pipelines.pipelines.rnaseq import Kallisto
+
+TEST_SAMPLE = 'GS-1'
+
+s3 = boto3.client('s3')
+
+t = Kallisto(sample_id=TEST_SAMPLE)
+for s3_tgt in t.output().values():
+	bucket, key = path_to_bucket_and_key(s3_tgt.path)
+	s3.delete_object(Bucket=bucket, Key=key)
+
+luigi.build([t], local_scheduler=True)
+
