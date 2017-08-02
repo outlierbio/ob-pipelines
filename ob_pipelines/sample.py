150dad224dd985762714b73e9a91d084efb11e06
Jake Feala <jake@outlierbio.com>
2017-08-02 11:03:28 -0400

    Update to match changes in ob-airtable

--- ob_pipelines/sample.py
+++ ob_pipelines/sample.py
@@ -1,20 +1,21 @@
 import os
 
 from luigi import Parameter
-from ob_airtable import get_record_by_name, get_record
+from ob_airtable import AirtableClient
 
 AIRTABLE_EXPT_TABLE = 'Genomics%20Expt'
 AIRTABLE_SAMPLE_TABLE = 'Genomics%20Sample'
 
 S3_BUCKET = os.environ.get('S3_BUCKET')
 
+client = AirtableClient()
 
 def get_samples(expt_id):
-    expt = get_record_by_name(expt_id, AIRTABLE_EXPT_TABLE)
+    expt = client.get_record_by_name(expt_id, AIRTABLE_EXPT_TABLE)
     sample_keys = expt['fields']['Genomics samples']
 
     for sample_key in sample_keys:
-        sample = get_record(sample_key, AIRTABLE_SAMPLE_TABLE)
+        sample = client.get_record(sample_key, AIRTABLE_SAMPLE_TABLE)
         yield sample['fields']['Name']
 
 
@@ -25,7 +26,7 @@ class Sample(object):
     @property
     def sample(self):
         if not hasattr(self, '_sample'):
-            self._sample = get_record_by_name(self.sample_id, AIRTABLE_SAMPLE_TABLE)['fields']
+            self._sample = client.get_record_by_name(self.sample_id, AIRTABLE_SAMPLE_TABLE)['fields']
         return self._sample
 
     @property
@@ -39,5 +40,5 @@ class Sample(object):
     def experiment(self):
         if not hasattr(self, '_experiment'):
             expt_key = self.sample['Experiment'][0]
-            self._experiment = get_record(expt_key, AIRTABLE_EXPT_TABLE)['fields']
+            self._experiment = client.get_record(expt_key, AIRTABLE_EXPT_TABLE)['fields']
         return self._experiment
