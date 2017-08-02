810fdf961da839a70fbc919e55497d7b6ecc9388
Jake Feala <jake@outlierbio.com>
2017-08-02 11:00:48 -0400

    Kallisto updates: allow bootstrapping, pipe-delimited transcript IDs

--- ob_pipelines/apps/kallisto/utils.py
+++ ob_pipelines/apps/kallisto/utils.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
 
-def load_abundance_gencode(fpath):
+def load_abundance_gencode(fpath, split_id):
     df = pd.read_csv(fpath, sep='\t')
-    target_attr = df.target_id.str.split('|', expand=True)
-    target_attr.columns = ['gencode_tx', 'gencode_gene', 'havana_gene', 'havana_tx', 
-        'hugo_tx', 'hugo_symbol', 'tx_length', 'gene_type', 'undefined']
-    merged = df.join(target_attr)
-    return merged[['gencode_tx', 'gencode_gene', 'hugo_symbol', 'gene_type', 
-        'length', 'eff_length', 'est_counts', 'tpm']]
+    if split_id:
+        target_attr = df.target_id.str.split('|', expand=True)
+        target_attr.columns = ['gencode_tx', 'gencode_gene', 'havana_gene', 'havana_tx', 
+            'hugo_tx', 'hugo_symbol', 'tx_length', 'gene_type', 'undefined']
+        merged = df.join(target_attr)
+        return merged[['gencode_tx', 'gencode_gene', 'hugo_symbol', 'gene_type', 
+            'length', 'eff_length', 'est_counts', 'tpm']]
+    else:
+        return df
 
-
-def merge_column(input_files, labels, data_col='est_counts'):
+def merge_column(input_files, labels, data_col='est_counts', annot=False):
     """Merge Kallisto samples into a matrix
 
     Gather one column from all input_files into a single merged
@@ -21,8 +23,11 @@ def merge_column(input_files, labels, data_col='est_counts'):
 
     # Grab annotations from the first file
     first = input_files[0]
-    first_df = load_abundance_gencode(first)
-    annotations = first_df[['gencode_tx', 'gencode_gene', 'hugo_symbol', 'gene_type', 'length']]
+    first_df = load_abundance_gencode(first, split_id=annot)
+    if annot:
+        annotations = first_df[['gencode_tx', 'gencode_gene', 'hugo_symbol', 'gene_type', 'length']]
+    else:
+        annotations = first_df[['target_id', 'length']]
 
     # Add the first file to the list
     sample_col = first_df[data_col]
@@ -31,7 +36,7 @@ def merge_column(input_files, labels, data_col='est_counts'):
 
     # Loop to add the rest
     for i, fpath in enumerate(input_files[1:]):
-        sample_df = load_abundance_gencode(fpath)
+        sample_df = load_abundance_gencode(fpath, split_id=annot)
         sample_col = sample_df[data_col]
         sample_col.name = labels[i + 1]
         all_data.append(sample_col)
@@ -39,7 +44,10 @@ def merge_column(input_files, labels, data_col='est_counts'):
     # Merge columns into a single matrix
     merged = pd.concat(all_data, axis=1)
 
-    # Use transcript ID as index
-    merged.index = annotations['gencode_tx']
+    if annot:
+        # Use Gencode Transcript ID as index
+        merged.index = annotations['gencode_tx']
+    else:
+        merged.index = annotations['target_id']
 
     return annotations, merged
