93838b44e57d75b653de8ebe707a6dcf5313aea5
Jake Feala <jake@outlierbio.com>
2017-10-24 12:44:19 -0400

    Pull Airtable parameters from config

--- ob_pipelines/sample.py
+++ ob_pipelines/sample.py
@@ -3,19 +3,16 @@ import os
 from luigi import Parameter
 from ob_airtable import AirtableClient
 
-AIRTABLE_EXPT_TABLE = 'Genomics%20Expt'
-AIRTABLE_SAMPLE_TABLE = 'Genomics%20Sample'
+from ob_pipelines.config import cfg
 
-S3_BUCKET = os.environ.get('S3_BUCKET')
-
-client = AirtableClient()
+client = AirtableClient(endpoint=cfg['AIRTABLE_API_ENDPOINT'], api_key=cfg['AIRTABLE_API_KEY'])
 
 def get_samples(expt_id):
-    expt = client.get_record_by_name(expt_id, AIRTABLE_EXPT_TABLE)
+    expt = client.get_record_by_name(expt_id, cfg['AIRTABLE_EXPT_TABLE'])
     sample_keys = expt['fields']['Genomics samples']
 
     for sample_key in sample_keys:
-        sample = client.get_record(sample_key, AIRTABLE_SAMPLE_TABLE)
+        sample = client.get_record(sample_key, cfg['AIRTABLE_SAMPLE_TABLE'])
         yield sample['fields']['Name']
 
 
@@ -26,13 +23,13 @@ class Sample(object):
     @property
     def sample(self):
         if not hasattr(self, '_sample'):
-            self._sample = client.get_record_by_name(self.sample_id, AIRTABLE_SAMPLE_TABLE)['fields']
+            self._sample = client.get_record_by_name(self.sample_id, cfg['AIRTABLE_SAMPLE_TABLE'])['fields']
         return self._sample
 
     @property
     def sample_folder(self):
         return '{expt}/{sample}'.format(
-            bucket=S3_BUCKET,
+            bucket=cfg['S3_BUCKET'],
             expt = self.experiment['Name'],
             sample=self.sample_id)
 
@@ -40,5 +37,5 @@ class Sample(object):
     def experiment(self):
         if not hasattr(self, '_experiment'):
             expt_key = self.sample['Experiment'][0]
-            self._experiment = client.get_record(expt_key, AIRTABLE_EXPT_TABLE)['fields']
+            self._experiment = client.get_record(expt_key, cfg['AIRTABLE_EXPT_TABLE'])['fields']
         return self._experiment
