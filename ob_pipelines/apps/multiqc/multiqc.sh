8f58ca493b32c2647efe1ca1916891bfe8952ac7
Jake Feala <jake@outlierbio.com>
2017-10-25 08:28:10 -0400

    MultiQC app

---
+++ ob_pipelines/apps/multiqc/multiqc.sh
@@ -0,0 +1,12 @@
+S3_FOLDER=$1
+LOCAL_FOLDER=$SCRATCH/multiqc/$(dirname $S3_FOLDER)
+
+mkdir -p $LOCAL_FOLDER
+aws s3 sync $S3_FOLDER $LOCAL_FOLDER \
+	--exclude="*.bam*" --exclude="*.fq*" --exclude="*.fastq*" \
+	--exclude="*mate*" --exclude="abundance.*"
+
+multiqc $LOCAL_FOLDER
+
+aws s3 cp multiqc_report.html $S3_FOLDER
+aws s3 cp multiqc_data/ $S3_FOLDER/multiqc_data/ --recursive
---
+++ ob_pipelines/apps/multiqc/test_aws_batch.py
@@ -0,0 +1,19 @@
+import os.path as op
+import dnli_pipelines
+from dnli_pipelines.batch import BatchClient
+
+module_dir = op.dirname(dnli_pipelines.__file__)  # __file__ is __init__.py, have to get parent folder
+
+bc = BatchClient()  # thin wrapper for boto3.client('batch')
+
+# Register job definition if needed
+job_def = op.join(module_dir, 'apps', 'multiqc', 'job-definition.json')
+bc.register_job_definition(job_def)
+
+parameters = {
+    'analysis_dir': 's3://com-dnli-ngs/test/1e4/'
+}
+
+job_id = bc.submit_job('multiqc', parameters)
+bc.get_job_status(job_id)
+bc.wait_on_job(job_id)
