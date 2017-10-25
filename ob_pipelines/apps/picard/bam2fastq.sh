3d18950804d57b9ed08ff5dec24e5590ab74d721
Jake Feala <jake@outlierbio.com>
2017-10-25 08:23:40 -0400

    Picard SamToFastQ

---
+++ ob_pipelines/apps/picard/bam2fastq.sh
@@ -0,0 +1,27 @@
+BAM=$1
+OUT1=$2
+OUT2=$3
+
+BAMFILE=$(basename $BAM)
+GZ1=$(basename $OUT1)
+GZ2=$(basename $OUT2)
+FQ1=${GZ1%.*}
+FQ2=${GZ2%.*}
+
+aws s3 cp $BAM $SCRATCH_DIR/$BAMFILE
+
+java -jar /picard.jar \
+	SamToFastq \
+	I=$SCRATCH_DIR/$BAMFILE \
+	FASTQ=$SCRATCH_DIR/$FQ1 \
+	SECOND_END_FASTQ=$SCRATCH_DIR/$FQ2
+
+gzip -c $SCRATCH_DIR/$FQ1 > $SCRATCH_DIR/$GZ1
+gzip -c $SCRATCH_DIR/$FQ2 > $SCRATCH_DIR/$GZ2
+
+aws s3 cp $SCRATCH_DIR/$GZ1 $OUT1
+aws s3 cp $SCRATCH_DIR/$GZ2 $OUT2
+
+rm $SCRATCH_DIR/$GZ1
+rm $SCRATCH_DIR/$GZ2
+rm $SCRATCH_DIR/$BAMFILE
---
+++ ob_pipelines/apps/picard/test_aws_batch.py
@@ -0,0 +1,22 @@
+import os.path as op
+
+import ob_pipelines
+from ob_pipelines.batch import BatchClient
+
+module_dir = op.dirname(ob_pipelines.__file__)  # __file__ is __init__.py, have to get parent folder
+
+bc = BatchClient()  # thin wrapper for boto3.client('batch')
+
+# Register job definition if needed
+job_def = op.join(module_dir, 'apps', 'picard', 'bam2fastq-job-definition.json')
+bc.register_job_definition(job_def)
+
+parameters = {
+    'input': 's3://c4t-data/GE-2/GS-31/disambiguate/GS-31.disambiguatedSpeciesA.bam',
+    'fq1': 's3://c4t-data/GE-2/GS-31/disambiguate/GS-31_1.fq.gz',
+    'fq2': 's3://c4t-data/GE-2/GS-31/disambiguate/GS-31_2.fq.gz'
+}
+
+job_id = bc.submit_job('bam2fastq', parameters)
+bc.get_job_status(job_id)
+bc.wait_on_job(job_id)
