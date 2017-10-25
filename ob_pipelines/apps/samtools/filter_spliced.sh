69c4f58dd0a88ae632ec84be28000fce558bb541
Jake Feala <jake@outlierbio.com>
2017-10-25 08:31:54 -0400

    Script for filtering spliced reads only

---
+++ ob_pipelines/apps/samtools/filter_spliced.sh
@@ -0,0 +1,13 @@
+IN=$1
+OUT=$2
+
+IN_LOCAL=$SCRATCH_DIR/$(basename $IN)
+OUT_LOCAL=$SCRATCH_DIR/$(basename $OUT)
+
+aws s3 cp $IN $IN_LOCAL
+
+samtools view -H $IN_LOCAL > $SCRATCH_DIR/header.txt
+samtools view $IN_LOCAL | awk '($6 ~ /N/)' > $SCRATCH_DIR/filtered.sam
+cat $SCRATCH_DIR/header.txt $SCRATCH_DIR/filtered.sam | samtools view -bS > $OUT_LOCAL
+
+aws s3 cp $OUT_LOCAL $OUT
