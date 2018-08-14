5c1ebd686a5b2e213b0b6270b1e9a4847ec0fe02
Alex Ulyanov <ulyanov.alexander@gmail.com>
2018-08-14 18:52:34 +0300

    change filter to load all job definition files to aws batch

--- scripts/register_job_defs.py
+++ scripts/register_job_defs.py
@@ -11,14 +11,18 @@ APPS_DIR = op.dirname(ob_pipelines.apps.__file__)
 batch_client = boto3.client('batch')
 
 for app in os.listdir(APPS_DIR):
-    job_def_file =  op.join(APPS_DIR, app, 'job-definition.json')
-    if not op.exists(job_def_file):
-        continue
-    print(job_def_file)
-    with open(job_def_file) as f:
-        job_def = json.load(f)
-    response = batch_client.register_job_definition(
-        jobDefinitionName=job_def['jobDefinitionName'],
-        type=job_def['type'],
-        containerProperties=job_def['containerProperties']
-    )
+    app_dir = op.join(APPS_DIR, app)
+    if os.path.isdir(app_dir):
+        for file in os.listdir(app_dir):
+            if file.endswith('definition.json'):
+                job_def_file = op.join(APPS_DIR, app, file)
+                print(job_def_file)
+                with open(job_def_file) as f:
+                    job_def = json.load(f)
+                response = batch_client.register_job_definition(
+                    jobDefinitionName=job_def['jobDefinitionName'],
+                    type=job_def['type'],
+                    containerProperties=job_def['containerProperties']
+                )
+            else:
+                continue
